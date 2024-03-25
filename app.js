bc377aef065c3594d97b4049cbb0825edf988cfe
greenleendeen <161842059+greenleendeen@users.noreply.github.com>
2024-03-25 09:39:34 +0100

    update 25/03 9h30

---
+++ app.js
@@ -0,0 +1,28 @@
+
+//j'appel mon grid à animer
+let grid= document.querySelector(".gridd-anim ")
+// j'ecoute event 'click' sur le 'grid'
+
+grid.addEventListener('click',()=>{
+
+    
+     // alert("Hello World!");
+
+      anime({
+        targets: '.gridd-anim .el',
+        scale: [
+          {value: .1, easing: 'easeOutSine', duration: 500},
+          {value: 1, easing: 'easeInOutQuad', duration: 1200}
+        ],
+        delay: anime.stagger(200, {grid: [3, 3], from: 'center'})
+      });
+})
+
+
+
+
+
+ 
+
+
+
